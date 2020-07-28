d4b61f8177faf786860bb03076b6a466a5e6b7e1
Kapil Sharma <ks2822123@gmail.com>
2020-07-28 22:14:41 +0530

    First Commit

---
+++ src/SingleNoteSummaryComponent.js
@@ -0,0 +1,39 @@
+import React from 'react';
+const { StyleSheet, Text, View } = require("react-native");
+
+
+
+const SingleNoteSummaryComponent = (props) => {
+  return (<View  backgroundColor={randomBackground()} style={styles.textViewStyle}>
+  <Text style={styles.textProperties}> {props.myNoteText}</Text>
+  </View>)
+};
+
+const randomBackground = () => {
+  var red = Math.floor(Math.random() * 255) // 123
+  var green = Math.floor(Math.random() * 255) // 45
+  var blue = Math.floor(Math.random() * 255) // 43
+
+  // String Interpolation
+  // In a string -> isnert a value of some other data type
+  // ""  ''  ``
+
+  return `rgb(${red}, ${green}, ${blue})` // rgb(123, 45, 43)
+};
+
+const styles = StyleSheet.create({
+  textProperties: {
+    fontSize: 24
+},
+textViewStyle: {
+    height: 150,
+    width: 150,
+    margin: 10,
+    borderRadius: 10,
+    padding: 5,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
+
+export default SingleNoteSummaryComponent;
